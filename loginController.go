fda5f4e1dc3ded20200a98a90a17832cc7b59634
Wanyoro John <jjohnie8@gmail.com>
2023-06-27 12:24:29 +0300

    clean up

--- loginController.go
+++ loginController.go
@@ -8,10 +8,6 @@ import (
 	"net/http"
 	"os"
 
-	//"time"
-
-	//"time"
-
 	"github.com/dgrijalva/jwt-go"
 	"golang.org/x/crypto/bcrypt"
 )
@@ -84,74 +80,12 @@ func (a *App) SignUp(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	userCreated, err := user.SavePerson(a.DB)
-	// if err != nil {
-	// 	formattedError := FormatError(err.Error())
-
-	// 	ERROR(w, http.StatusInternalServerError, formattedError)
-	// 	return
-	// }
 	w.Header().Set("Location", fmt.Sprintf("%s%s/%d", r.Host, r.RequestURI, userCreated.ID))
 	JSON(w, http.StatusCreated, userCreated)
 }
 
 func (a *App) Login(w http.ResponseWriter, r *http.Request) {
-	// var credentials Credentials
-	// var dbUser Person
-	// var resp = map[string]interface{}{"status": "success", "message": "logged in"}
 
-	// err := json.NewDecoder(r.Body).Decode(&credentials)
-	// if err != nil {
-	// 	w.WriteHeader(http.StatusBadRequest)
-	// 	return
-	// }
-
-	// dbName := a.DB.Debug().Find(&dbUser, "fullname", credentials.Fullname)
-	// fmt.Printf("%s", dbUser.Password)
-
-	// if dbName.RowsAffected == 0 {
-	// 	w.WriteHeader(http.StatusBadRequest)
-	// 	return
-	// }
-
-	// err = bcrypt.CompareHashAndPassword([]byte(dbUser.Password), []byte(credentials.Password))
-	// if err != nil {
-	// 	ERROR(w, http.StatusUnauthorized, err)
-	// 	fmt.Printf("%s", err)
-	// 	return
-	// }
-
-	// fmt.Println("password matched")
-
-	// expirationTime := time.Now().Add(time.Minute * 5)
-
-	// claims := &Claims{
-	// 	Fullname: credentials.Fullname,
-	// 	StandardClaims: jwt.StandardClaims{
-	// 		ExpiresAt: expirationTime.Unix(),
-	// 	},
-	// }
-	// token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	// tokenString, err := token.SignedString(jwtKey)
-	// if err != nil {
-	// 	w.WriteHeader(http.StatusInternalServerError)
-	// 	return
-	// }
-
-	// http.SetCookie(w,
-	// 	&http.Cookie{
-	// 		Name:    "token",
-	// 		Value:   tokenString,
-	// 		Expires: expirationTime,
-	// 	})
-	// token, err := AuthToken(dbUser.ID)
-	// fmt.Printf("%s", token)
-	// if err != nil {
-	// 	ERROR(w, http.StatusBadRequest, err)
-	// 	return
-	// }
-	// resp["token"] = token
-	// JSON(w, http.StatusOK, resp)
-	// return
 	w.Header().Set("Content-Type", "Application/json")
 	var resp = map[string]interface{}{"status": "success", "message": "logged in"}
 
